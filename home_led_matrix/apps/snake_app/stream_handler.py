b0542d7f51a9cb76156b1631ef08c4442677c9da
Jesper Fritsch <jesperf96@gmail.com>
2025-01-20 19:33:00 +0100

    Adjusting min batch size

--- home_led_matrix/apps/snake_app/stream_handler.py
+++ home_led_matrix/apps/snake_app/stream_handler.py
@@ -38,7 +38,7 @@ class StreamHandler:
     def __init__(self) -> None:
         self._websocket = None
         self._min_buffer_size = 30
-        self._min_batch_size = 10
+        self._min_batch_size = 1
         self._recieved_data: deque[StepPixelChangesData] = deque()
         self._staging_data: Dict[int, StepPixelChangesData] = {}
         self._init_data = None
