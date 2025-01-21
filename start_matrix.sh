0913be3af4bb007c725c80a2ec853534f392c5d9
Jesper Fritsch <jesperf96@gmail.com>
2025-01-21 23:06:17 +0100

    Fix

--- start_matrix.sh
+++ start_matrix.sh
@@ -2,5 +2,5 @@
 source /home/jesper/snake_server/server_venv/bin/activate
 
 umask 000
-python -m home_led_matrix.main
+/home/jesper/snake_server/server_venv/bin/python -m home_led_matrix.main
 deactivate
